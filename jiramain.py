0855b1269c14eec861612721ca6ce666de2a19bf
padlik <propan@gmail.com>
2015-07-08 18:44:53 +0300

    Changed bug in dates comparision

--- jiramain.py
+++ jiramain.py
@@ -9,6 +9,7 @@ from paramsparser import AppParamsParser
 import inject
 from injectors import SQLDb
 from injectors import Jira
+from primitives.logger import Logger
 
 config = None
 
@@ -35,6 +36,7 @@ def do_inject():
         binder.bind(SQLDb, sql_db)
         binder.bind(Jira, jira)
 
+    Logger.debug("Injection complete")
     inject.configure(my_config)
 
 
@@ -60,27 +62,34 @@ def report_worker(user):
     def jira2date(date_string):
         import iso8601
         import datetime
+
         complex_date = iso8601.parse_date(date_string)
         simple_date = datetime.datetime(year=complex_date.year, month=complex_date.month, day=complex_date.day)
         return simple_date
 
     jira = inject.instance(Jira)
     start_date, finish_date = get_dates()
+    Logger.debug("Dates to query from {} to {}".format(start_date, finish_date))
     qry = 'key in workedIssues("{start}", "{finish}", "{user}")'.format(start=start_date.strftime("%Y/%m/%d"),
                                                                         finish=finish_date.strftime("%Y/%m/%d"),
                                                                         user=user)
+    Logger.debug("Query is {}".format(qry))
     issues = jira.search_issues(qry)
     ts = []
     for i in issues:
         for w in [w for w in jira.worklogs(i.key) if w.author.name == user]:
 
             d_created = jira2date(w.started)
-            if start_date >= d_created <= finish_date:  # it might happens too!
+            if start_date <= d_created <= finish_date:  # it might happens too!
                 ts.append([w.id, w.comment, w.timeSpentSeconds, d_created])
+    Logger.debug("Timesheets for user {} are {}".format(user, ts))
     return {user: ts}
 
 
 if __name__ == "__main__":
     config = do_parse(sys.argv[1:])
+    from primitives.logger import set_logging
+
+    set_logging(config)
     do_inject()
-    print report_worker("mak@intetics.com")
+    print report_worker("p.pronko@intetics.com")
