b7e78a4b5e2975eb196ca51bb59c9901ca01959d
Paul  Pronko <propan@gmail.com>
2017-06-26 13:27:32 +0300

    Very first try of heroku deplloyment

--- db_exp.py
+++ db_exp.py
@@ -1,5 +1,7 @@
 from datasources import SQLDataSource, mysql_creator
 from payloads import SugarPayload2
+from serializers import TimeSheet, User
+from sqlalchemy.sql import func
 
 
 def get_dates(year, month):
@@ -18,11 +20,11 @@ def get_dates(year, month):
 if __name__ == '__main__':
     SQLDataSource.set_creator(mysql_creator)
     mysql = SQLDataSource.instance
-    # dates = get_dates(2017, 4)
+    dates = get_dates(2017, 4)
+    us = [49L, 59L]
     # print dates
-    # users = mysql.query(TimeSheet).filter(TimeSheet.activity_date >= dates[0],
-    #                                       TimeSheet.activity_date <= dates[1],
-    #                                       TimeSheet.source.is_(None)).count()
-    # print users
-
-    print SugarPayload2.check_users_sugarid(check_dismissed=True)
+    s = mysql.query(TimeSheet.userid, func.sum(TimeSheet.time_spent).label('cnt')).group_by(TimeSheet.userid).subquery()
+    for u, c in mysql.query(User, s.c.cnt).outerjoin(s, User.id == s.c.userid).order_by(User.sugar_uname):
+        print (u.sugar_uname, c)
+    # users = mysql.query(User).join(TimeSheet).filter(TimeSheet.time_spent > 100)
+    # print users.all()
