e3eaba9056936539f8e572c7bad3d578334d3da1
ppronko-sugarcrm <ppronko@sugarcrm.com>
2021-07-06 19:28:33 +0300

    Removed postres-specific data load from run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
   RETRIES=10
 
   echo "Connecting to ${DATABASE_URL}"
-  until psql ${DATABASE_URL} -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  until psql "${DATABASE_URL}" -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
     echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
     sleep 10
   done
